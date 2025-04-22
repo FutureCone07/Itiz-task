b9f1ba2df0cc6138538a4f8ba77636333acf68de
FutureCone07 <angelromeroramos3@gmail.com>
2025-04-22 11:55:03 -0600

    Commit rama dobleR actualizacion de código

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(androidx.recyclerview:recyclerview:1.1.0)
+    implementation(com.google.android.material:material:1.1.0)
+
 }
--- app/src/main/java/com/example/itiz_task/Item.java
+++
@@ -1,4 +0,0 @@
-package com.example.itiz_task;
-
-public class Item {
-}
